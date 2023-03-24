3aa96459d83da8a893b9ed53eeaa580c0539f7db
yuanyxh <72425622+yuanyxh@users.noreply.github.com>
2023-03-24 08:38:27 +0000

    Deploying to master from @ yuanyxh/introduce@7f7ce9da34ca02ed2a9d62cf3b4aee991ed6ba99 🚀

---
+++ _next/static/ayVDT1DnV5SfjPqsVIQIK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0bc0cde260d08b97.css","static/chunks/pages/index-6f18dccfc4c9a80d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
